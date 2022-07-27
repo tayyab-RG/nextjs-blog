b52fa36c281aaba51858da81076744c6a72f999e
Tayyab Nadeem <tayyab@revolvinggames.com>
2022-07-27 10:57:48 +0500

    links

--- pages/newpage.js
+++ pages/newpage.js
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function newPage() {
-    return (<h1>New Page!</h1>);
+    return (
+        <>
+            <h1>New Page!</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back To Home!</a>
+                </Link>
+            </h2>
+        </>        
+    );
 }
