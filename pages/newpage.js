60daaabcb65ba41d6f7dd327555246698219d571
Tayyab Nadeem <tayyab@revolvinggames.com>
2022-07-27 11:53:36 +0500

    next-image

--- pages/newpage.js
+++ pages/newpage.js
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import Image from 'next/image'
+
 
 export default function newPage() {
     return (
@@ -9,6 +11,13 @@ export default function newPage() {
                     <a>Back To Home!</a>
                 </Link>
             </h2>
+            {/* <img src="/images/profile.png" width="1440" height="1440" alt="Your Name" /> */}
+            <Image
+                src={"/images/profile.png"}
+                width={1440}
+                height={1440}
+                alt={"Name is Khan"}
+            />
         </>        
     );
 }
